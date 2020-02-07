1ab9b05096e9b13ac61ac22a54ed8b423143ef1d
angrykoala <angrykoala@outlook.es>
2020-02-07 17:50:56 +0100

    Readme and some comments

--- README.md
+++ README.md
@@ -2,33 +2,7 @@ Koalafied Typescript
 ====================
 _by @angrykoala_
 
->Use this repo as a template for your Typescript projects.
-
->Add Description Here
-
-**Features**
-* tsconfig reasonable configuration
-* testing with mocha ready to go
-* tslint
-* Travis CI integration
-* Test Coverage
-
-## Getting Started
-
-> How to use your project
-
-Having `node` and `npm` installed:
-1. `npm install`
-2. `npm test`
-3. `npm start`
-
-## Examples
-
-> Usage Examples
-
-## Development
-
-> How to develop your project
+Tests if an string is an xPath. This package does not provide full guarantee that the xPath is valid. This package is intended for code trying to check if a string is an xPath selector or css selector.
 
 ## License
 This projects is under GPL-3.0 License
--- main.ts
+++ main.ts
@@ -1,12 +1,18 @@
+const VALID_AXIS = ["ancestor", "ancestor-or-self", "attribute", "child", "descendant", "descendant-or-self",
+"following", "following-sibling", "namespace", "parent", "preceding", "preceding-sibling", "self"];
+
+
 export function isXPathQuery(s: string): boolean {
+    // Test for @ and plain nodes
     if (s[0] === '/') return true;
     if (/^.\./.test(s)) return true;
     const axisSplit = s.split("::");
     if (axisSplit.length > 1) {
-        const validAxis = ["ancestor", "ancestor-or-self", "attribute", "child", "descendant", "descendant-or-self",
-            "following", "following-sibling", "namespace", "parent", "preceding", "preceding-sibling", "self"];
         const axe = axisSplit[0];
-        if (validAxis.indexOf(axe) !== -1) return true;
+        if (VALID_AXIS.indexOf(axe) !== -1) return true;
     }
     return false;
 }
+
+
+// TO CHECK: Invalid .//header/
