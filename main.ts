7927264b9713cd48331fa91a3ec34cbe700feae4
angrykoala <angrykoala@outlook.es>
2020-02-07 17:32:21 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,12 @@
-import Example from './src/example';
-
-const example = new Example();
-
-console.log(example.myMethod());
+export function isXPathQuery(s: string): boolean {
+    if (s[0] === '/') return true;
+    if (/^.\./.test(s)) return true;
+    const axisSplit = s.split("::");
+    if (axisSplit.length > 1) {
+        const validAxis = ["ancestor", "ancestor-or-self", "attribute", "child", "descendant", "descendant-or-self",
+            "following", "following-sibling", "namespace", "parent", "preceding", "preceding-sibling", "self"];
+        const axe = axisSplit[0];
+        if (validAxis.indexOf(axe) !== -1) return true;
+    }
+    return false;
+}
