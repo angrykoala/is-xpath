6e615cde54f191e5f7517233358debb81579a421
angrykoala <angrykoala@outlook.es>
2020-02-08 19:56:03 +0100

    more tests

--- README.md
+++ README.md
@@ -2,7 +2,9 @@ Koalafied Typescript
 ====================
 _by @angrykoala_
 
-Tests if a string is a [xPath](https://en.wikipedia.org/wiki/XPath).
+Tests if a string is a [xPath](https://en.wikipedia.org/wiki/XPath). This is **not** a full xPath validation, but a simple way of figuring out if a given string is an xPath or css selector.
+
+* Works on node without DOM.
 
 
 > **Warning:** This package does not provide full guarantee that the xPath is valid. This package is intended for code trying to check if a string is an xPath selector or css selector.
--- tests/main.test.js
+++ tests/main.test.js
@@ -3,11 +3,20 @@
 const {assert} = require('chai');
 const isXPath = require('..');
 
-describe("Is XPath", () => {
-    it("Valid xPath", () => {
-        assert.isTrue(isXPath("/p"));
-    });
-    it("Not xPath", () => {
-        assert.isFalse(isXPath(".p"));
-    });
+const validPaths = require('./config/valid_paths.json');
+const invalidPaths = require('./config/invalid_paths.json');
+
+describe("Valid XPaths", () => {
+    for (const path of validPaths) {
+        it(`${path} is XPath`, () => {
+            assert.isTrue(isXPath(path), `Expected ${path} to be valid.`);
+        });
+    }
+});
+describe("InValid XPaths", () => {
+    for (const path of invalidPaths) {
+        it(`${path} is not XPath`, () => {
+            assert.isFalse(isXPath(path), `Expected ${path} to not be valid.`);
+        });
+    }
 });
