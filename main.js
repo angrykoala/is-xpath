45617da02eb247ff107253b1bc52fd9c0d9c819b
angrykoala <angrykoala@outlook.es>
2020-02-08 20:08:28 +0100

    more tests and minor code improvements

--- main.js
+++ main.js
@@ -3,16 +3,21 @@
 const VALID_AXIS = ["ancestor", "ancestor-or-self", "attribute", "child", "descendant", "descendant-or-self",
     "following", "following-sibling", "namespace", "parent", "preceding", "preceding-sibling", "self"];
 
-module.exports = function isXPath(s) {
-    // Test for @ and plain nodes
-    if (s[0] === '/') return true;
-    if (/^.\./.test(s)) return true;
+
+function firstAxisIsValid(s){
     const axisSplit = s.split("::");
     if (axisSplit.length > 1) {
         const axe = axisSplit[0];
         if (VALID_AXIS.indexOf(axe) !== -1) return true;
     }
+}
+
+
+
+module.exports = function isXPath(s) {
+    // Test for @ and plain nodes
+    if (s[0] === '/') return true;
+    if (/^.\./.test(s)) return true;
+    if(firstAxisIsValid(s)) return true;
     return false;
 };
-
-// TO CHECK: Invalid .//header/
