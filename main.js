61709c45aae82213a1ba3232a720a73a93bab67d
angrykoala <angrykoala@outlook.es>
2020-02-09 00:54:00 +0100

    lint fix

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const VALID_AXIS = ["ancestor", "ancestor-or-self", "attribute", "child", "desce
     "following", "following-sibling", "namespace", "parent", "preceding", "preceding-sibling", "self"];
 
 
-function firstAxisIsValid(s){
+function firstAxisIsValid(s) {
     const axisSplit = s.split("::");
     if (axisSplit.length > 1) {
         const axe = axisSplit[0];
@@ -13,11 +13,10 @@ function firstAxisIsValid(s){
 }
 
 
-
 module.exports = function isXPath(s) {
     // Test for @ and plain nodes
     if (s[0] === '/') return true;
     if (/^.\./.test(s)) return true;
-    if(firstAxisIsValid(s)) return true;
+    if (firstAxisIsValid(s)) return true;
     return false;
 };
